f0b57cdf87d9d44972e284011e3c3a95f2e837b6
nconsigny <nicolas.consigny@ethereum.org>
2025-03-26 03:26:14 +0100

    fix uri missmatch

--- .github/ACDbot/scripts/get_refresh_token.py
+++ .github/ACDbot/scripts/get_refresh_token.py
@@ -7,7 +7,7 @@ SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 flow = InstalledAppFlow.from_client_secrets_file(
     "client_secrets.json",  # Download from Google Cloud Console
     scopes=SCOPES,
-    redirect_uri="http://localhost:8080"  # Use localhost instead of OOB
+    redirect_uri="http://localhost:8080/"  # Use localhost instead of OOB
 )
 
 # Generate authorization URL
